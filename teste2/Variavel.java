18a298012e7d26a636b747908fd7795285764a85
Luiz <luizgustav796@gmail.com>
2025-06-07 20:23:15 -0300

    Variaveis em java
    testando e criando variaveis em java

---
+++ teste2/Variavel.java
@@ -0,0 +1,13 @@
+package teste2;
+/* 1- Use sempre PACKAGE para especificar o caminho do pacote/pasta
+   3- use o nome do arquivo quando for declarar uma classe se não fudeu */
+public class Variavel {
+  public static void main(String[] args) {
+    String name = "  luiz /idade: ";
+    int idade =  19  ;
+    String endereco ="  /Rua dos alpelinos 345/"  ;
+    System.out.print(name);
+    System.out.print(idade);
+    System.out.print(endereco);
+  }
+}
