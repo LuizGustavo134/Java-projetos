3f75f696407f3c33f4615d9c3fbb4e412db24087
Luiz <luizgustav796@gmail.com>
2025-06-07 19:04:40 -0300

    teste.java
    introdução ao java

---
+++ teste.java/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("hello world");
+        // esse comando imprime um texto quebrando uma linha no final
+        System.out.print("hello world");
+        // print não adiciona uma nova linha no final
+        /* comentário de várias linhas
+        System.out.println("+hello world");   
+         * ojiji
+         * dddd
+         * dd
+         * d
+         * dd
+         * d
+         * d
+         * dd
+         * 
+         * d
+         * d
+         * dd
+         * 
+         * d
+         * d
+         * d
+         * d
+         * 
+         * 
+        */
+
+}}
