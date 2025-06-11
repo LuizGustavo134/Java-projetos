e74fdeb3bb55bd32485eb00f8f6e298a050474b0
Luiz <luizgustav796@gmail.com>
2025-06-11 17:43:26 -0300

    .

---
+++ cont/contar.java
@@ -0,0 +1,10 @@
+package cont;
+
+public class contar {
+    public static void main(String[] args){
+    int contador= 20;
+    contador = contador + 1;
+    System.out.println(contador);
+    }
+   
+}
