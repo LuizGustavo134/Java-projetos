49053a88660b05d670badf606dded287dc2289d7
Dev-Lu <159707892+LuizGustavo134@users.noreply.github.com>
2025-08-05 10:46:13 -0300

    Aula de projeto de software
    primeira aula de projeto de software

---
+++ Projeto_software_aula1/Variaveis.java
@@ -0,0 +1,17 @@
+package Projeto_software_aula1;
+
+public class Variaveis {
+    public static void main(String[]args) {
+    int numero1 = 5;
+    int numero2 = 10;
+
+    int soma = numero1 + numero2;
+    int subtracao = numero1 - numero2;
+    int multiplicacao = numero1 * numero2;
+    double divisao = (double) numero1 % numero2;
+        System.out.println("soma "+soma);
+        System.out.println("subtracao: "+subtracao);
+        System.out.println("multiplicacao: "+multiplicacao);
+        System.out.println("divisao: "+divisao);
+    }
+}
---
+++ entrada_de_dados/Dados.java
@@ -0,0 +1,20 @@
+package entrada_de_dados;
+import java.util.Scanner;
+import java.math.
+public class Dados {
+    public static void main(String[] args) {
+        {
+            Scanner entrada = new Scanner(System.in);
+            String nome;
+            System.out.println("Digite seu nome: ");
+            nome = entrada.nextLine();
+
+            System.out.println("Digite sua idade: ");
+            int nota = entrada.nextInt();
+
+            System.out.println("Dados: " + nome + ", " + nota   );
+             double area
+            entrada.close();
+        }
+    }
+}
