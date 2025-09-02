72b0db98d2f9f7b5c977ac71508a85978b15b96b
Dev-Lu <159707892+LuizGustavo134@users.noreply.github.com>
2025-09-02 10:59:54 -0300

    aula5
    aula5

---
+++ Aula5/Array.java
@@ -0,0 +1,9 @@
+package Aula5;
+import java.util.ArrayList;
+import java.util.List;
+public class Array {
+    public static void main(String[]args){
+
+        Array lista_nomes = new Array();
+    }
+}
---
+++ Aula5/Main.java
@@ -0,0 +1,12 @@
+package Aula5;
+
+public class Main {
+    // criando vetores
+    //vetores são como tuplas mas editaveis
+    double[] alunos = new  double[3];{
+        alunos[0] = 1.0;
+        alunos[1] = 2.0;
+        alunos[2] = 3.0;
+    }
+
+    }
